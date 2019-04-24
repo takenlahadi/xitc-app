ff4271bba8d49d4115eb8b2a8980c456964a6d21
Tahir <mtahir@xl.co.id>
2019-04-24 11:59:33 +0700

    Activity and Intent

---
+++ app/src/main/java/com/xl/xitcapp/LoginActivity.java
@@ -0,0 +1,52 @@
+package com.xl.xitcapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText edtUsername;
+    EditText edtPassword;
+//    Button btnClick;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        edtUsername = findViewById(R.id.edtUsername);
+        edtPassword = findViewById(R.id.edtPassword);
+        /*
+        btnClick = findViewById(R.id.btn_login);
+        btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });*/
+    }
+
+    public void doLogin(View view) {
+        String strUsername = edtUsername.getText().toString();
+        String strPassword = edtPassword.getText().toString();
+
+        if (strUsername.equals("admin") && strPassword.equals("password")) {
+//            Toast.makeText(this, "Username dan password BENAR", Toast.LENGTH_LONG).show();
+            Bundle b = new Bundle();
+            Intent i = new Intent(LoginActivity.this, MainActivity.class);
+            b.putString("USERNAME", strUsername);
+            b.putString("PASSWORD", strPassword);
+//            i.putExtra("USERNAME", strUsername);
+            i.putExtras(b);
+            startActivity(i);
+        } else {
+            Toast.makeText(this, "Username dan password SALAH", Toast.LENGTH_LONG).show();
+        }
+    }
+
+}
--- app/src/main/java/com/xl/xitcapp/MainActivity.java
+++ app/src/main/java/com/xl/xitcapp/MainActivity.java
@@ -2,12 +2,25 @@ package com.xl.xitcapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtHello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Bundle b = getIntent().getExtras();
+
+//        String username = getIntent().getStringExtra("USERNAME");
+        String username = b.getString("USERNAME");
+        String password = b.getString("PASSWORD");
+
+        txtHello = findViewById(R.id.txtHello);
+        txtHello.setText("Username Anda: "+username+" dan Password: "+password);
     }
+
 }
